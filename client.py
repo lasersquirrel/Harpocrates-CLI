c79998075c9fadb06652804c4cdbffbecff7ef7c
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2020-02-29 17:46:22 +0000

    Set up initial connection code (username exchange, second public key sending)

--- client.py
+++ client.py
@@ -1,5 +1,105 @@
 # Created by lasersquirrel on 29/02/2020.
 # Licensed under MIT.
-from encwork.client import client
+from encwork.client import Client
+from encwork.encryption import *
+from threading import Thread
 
+global ready_to_send
+global states
+global current_state
+ready_to_send = -1 # Track whether or not the client is ready to send messages
+states = {0: "", 1: ""} # Track whether or not the server confirmed an initialization action
+current_state = 0 # Avoid overwriting states dict
+
+TOKEN_FILE = "harpocrates-tk.txt"
+# Check for token file (used to reserve username)
+try:
+    with open(TOKEN_FILE, "r") as f:
+        tk = f.read()
+except FileNotFoundError:
+    tk = None
+
+# Main code
+class StatusThread(Thread):
+    def __init__(self, client):
+        Thread.__init__(self)
+        self._client = client
+    
+    def run(self):
+        global ready_to_send
+        global states
+        global current_state
+        for status in client.statuses(1):
+            if status["code"] == 11: # Public key received
+                ready_to_send += 1
+            elif status["code"] == 15: # Public key sent
+                ready_to_send += 1
+            elif status["code"] == 8: # Message received
+                # Check progress of connection
+                if current_state == 0:
+                    # Check the server's reply
+                    states[0] = status["data"][0]
+                    if status["data"][0][:2] == "OK":
+                        current_state += 1
+
+                elif current_state == 1:
+                    # Check the server's reply
+                    states[1] = status["data"][0]
+                    if status["data"][0][:2] == "OK":
+                        current_state += 1
+                
 client = Client()
+StatusThread(client).start()
+target = input("Enter the server IP: ")
+client.start(target)
+
+# Wait for the key exchange
+while ready_to_send < 1:
+    pass
+
+# Send token or username
+while True:
+    if tk is None:
+        # Get username if there is no token saved
+        uname = input("Username to use: ")
+        client.send_msg("uname" + uname)
+        method_used = "USERNAME" # Track whether a token or username was sent to server
+    else:
+        # Send token to server
+        client.send_msg("token" + tk)
+        method_used = "TOKEN"
+
+    # Wait for server reply
+    while states[0] == "":
+        pass
+    # Check if the username/token was confirmed
+    if states[0][:2] == "OK": # Username free/token is fine
+        if method_used == "USERNAME":
+            # Save new token to file
+            with open(TOKEN_FILE, "w") as f:
+                f.writelines(states[0][2:])
+        break
+    else:
+        if method_used == "TOKEN":
+            print(f"Invalid username token in {TOKEN_FILE}.")
+            exit()
+        elif method_used == "USERNAME":
+            print("Username is taken.")
+
+# Send custom public key
+pk = gen_private_key()
+while True:
+    client.send_msg(get_public_key_text(get_public_key(pk)).decode("ascii"))
+    # Wait for server reply
+    while states[1] == "":
+        pass
+    # Check if the public key was confirmed
+    if states[1][:2] == "OK":
+        print("Custom public key confirmed.")
+        break
+    else:
+        print("Public key was rejected by server.")
+        exit()
+
+# Send target user
+target_uname = input("Enter the username to connect to: ")
--- server.py
+++ server.py
@@ -1,6 +1,98 @@
 # Created by lasersquirrel on 29/02/2020.
 # Licensed under MIT.
-from encwork.server import server
+from encwork.server import Server
+from encwork.encryption import *
+import binascii
+import os
+from json import loads, dumps
+from json.decoder import JSONDecodeError
+
+# Track connections & pairs
+connections = []
+paired = []
+# Public keys of those connected (ip: key)
+public_keys = {}
+# Track how far into the connection process each client is (username setting, etc)
+connection_process = {}
+# Reserve usernames to IP's
+USERNAMES_FILE = "harpocrates-usernames.json" # File to store usernames/IP's
+
+# Load usernames
+try:
+    with open(USERNAMES_FILE, "r") as f:
+        usernames = loads(f.read())
+except FileNotFoundError: # Create file if it doesn't exist
+    print("No usernames file, creating one...")
+    with open(USERNAMES_FILE, "w") as f:
+        f.writelines("{}")
+    usernames = {}
+    print("Done.")
+except JSONDecodeError: # Invalid file
+    while True:
+        ok = input("Invalid usernames file, OK to overwrite? [y/n]: ")
+        if ok.lower() == "y":
+            with open(USERNAMES_FILE, "w") as f:
+                f.writelines("{}")
+            usernames = {}
+            print("Done.")
+            break
+        elif ok.lower() == "n":
+            exit()
+
 
 server = Server()
 server.start()
+
+# Loop
+for status in server.statuses(2.5):
+    # Log all statuses & data
+    print(f"[{status['code']}] {status['data']}")
+
+    if status["code"] == 11: # Public key received
+        connections.append(status["data"])
+        connection_process[status["data"]] = 0
+    if status["code"] == 8: # Message received
+        # Check connection progress of the client
+        if connection_process[status["data"][1]] == 0: # Client username-getting
+            # Client has provided a token
+            if status["data"][0][:5] == "token":
+                # Check if the token exists
+                if status["data"][0][5:] in usernames: # Valid token
+                    # Tell the client the token is valid
+                    server.send_msg("OK", status["data"][1])
+                    connection_process[status["data"][1]] += 1
+                else: # Invalid token
+                    # Tell the client to token is invalid
+                    server.send_msg("NO", status["data"][1])
+            
+            # Client has provided a new username
+            if status["data"][0][:5] == "uname":
+                # Check if the username is taken
+                if status["data"][0][5:] not in usernames.values(): # Untaken username
+                    # Generate a new token
+                    tk = binascii.b2a_hex(os.urandom(64)).decode("ascii")
+                    # Save the token
+                    usernames[tk] = status["data"][0][5:]
+                    with open(USERNAMES_FILE, "w") as f:
+                        f.write(dumps(usernames))
+                    # Tell the client the username is valid and provide a token for future use
+                    server.send_msg("OK" + tk, status["data"][1])
+                    connection_process[status["data"][1]] += 1
+                else: # Taken username
+                    # Tell the client the username has been taken
+                    server.send_msg("NO", status["data"][1])
+        
+        elif connection_process[status["data"][1]] == 1: # Client key-getting
+            # Test the key and send response
+            try:
+                encrypt(b"OK", status["data"][0].encode("ascii"))
+                public_keys[status["data"][1]] = status["data"][0].encode("ascii")
+                server.send_msg("OK", status["data"][1])
+                connection_process[status["data"][1]] += 1
+            except Exception as e:
+                print(e)
+                # Failed
+                server.send_msg("NO", status["data"][1])
+        
+        elif connection_process[status["data"][1]] == 2: # Client target-getting
+            pass
